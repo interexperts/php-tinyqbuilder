60184b39cdf05d3ff56417262b3439e6cb1c056c
Niels de Blaauw <n.de.blaauw@interexperts.nl>
2016-02-18 14:19:42 +0100

    Implementation tests afgemaakt

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -18,12 +18,84 @@ class BuilderTest extends PHPUnit_Framework_TestCase{
 		);
 	}
 
-	public function testCanReset(){
+	public function testImplementsAnd(){
+		$builder = new Builder();
+		$builder->and();
+		$this->assertEquals('.a', $builder->build());
+	}
+
+	public function testImplementsEqual(){
+		$builder = new Builder();
+		$builder->equal('name', 'test');
+		$this->assertEquals('name.etest', $builder->build());
+	}
+
+	public function testImplementsGreater(){
+		$builder = new Builder();
+		$builder->greater('name', 15);
+		$this->assertEquals('name.g15', $builder->build());
+	}
+
+	public function testImplementsGreaterOrEqual(){
+		$builder = new Builder();
+		$builder->greaterOrEqual('name', 15);
+		$this->assertEquals('name.h15', $builder->build());
+	}
+
+	public function testImplementsLess(){
+		$builder = new Builder();
+		$builder->less('name', 15);
+		$this->assertEquals('name.l15', $builder->build());
+	}
+
+	public function testImplementsLessOrEqual(){
+		$builder = new Builder();
+		$builder->lessOrEqual('name', 15);
+		$this->assertEquals('name.m15', $builder->build());
+	}
+
+	public function testImplementsLike(){
+		$builder = new Builder();
+		$builder->like('name', 'test');
+		$this->assertEquals('name.ktest', $builder->build());
+	}
+
+	public function testImplementsLParen(){
+		$builder = new Builder();
+		$builder->lparen();
+		$this->assertEquals('.s', $builder->build());
+	}
+
+	public function testImplementsNot(){
+		$builder = new Builder();
+		$builder->not();
+		$this->assertEquals('.t', $builder->build());
+	}
+
+	public function testImplementsNotEqual(){
+		$builder = new Builder();
+		$builder->notEqual('name', 'test');
+		$this->assertEquals('name.ntest', $builder->build());
+	}
+
+	public function testImplementsOr(){
+		$builder = new Builder();
+		$builder->or();
+		$this->assertEquals('.o', $builder->build());
+	}
+
+	public function testImplementsReset(){
 		$builder = new Builder();
 		$builder->greater('x', 10);
 		$this->assertNotEmpty($builder->build());
 		$builder->reset();
 		$this->assertEmpty($builder->build());
+	}
+
+	public function testImplementsRParen(){
+		$builder = new Builder();
+		$builder->reparen();
+		$this->assertEquals('.f', $builder->build());
 	}
 
 	public function testThrowOnIllegalMethod(){
